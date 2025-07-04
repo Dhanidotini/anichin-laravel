f0300024acdf220c45c9525e14d411d841e80bc0
Dhanidotini <apokta2006@gmail.com>
2025-07-04 17:08:41 +0700

    change frontend to be api

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('welcome', 'welcome')->name('welcome');
